757558727ce4bcd975aa4fa278e6daf2a5c3ab0a
NHTD12502 <118121155+NHTD12502@users.noreply.github.com>
2025-04-25 01:07:50 +0700

    *** important
    - Remove color jitter

--- utils/dataset/isic.py
+++ utils/dataset/isic.py
@@ -108,9 +108,9 @@ augmentation_rand = transforms_v2.Compose(
     [
         transforms_v2.Resize((224,224)),
         transforms_v2.RandomHorizontalFlip(p=0.5),
-        transforms_v2.RandomApply([
-                transforms_v2.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
-            ], p=0.8),
+        # transforms_v2.RandomApply([
+        #         transforms_v2.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+        #     ], p=0.8),
         
         transforms_v2.ToTensor(),
         # transforms_v2.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))    
@@ -123,9 +123,9 @@ augmentation_sim = transforms_v2.Compose(
         transforms_v2.RandomHorizontalFlip(p=0.5),
         transforms_v2.RandomVerticalFlip(p=0.5),
         transforms_v2.RandomRotation(90),
-        transforms_v2.RandomApply([
-                transforms_v2.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
-            ], p=0.8),
+        # transforms_v2.RandomApply([
+        #         transforms_v2.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+        #     ], p=0.8),
         transforms_v2.RandomGrayscale(p=0.2),
         transforms_v2.ToTensor(),
         # transforms_v2.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
